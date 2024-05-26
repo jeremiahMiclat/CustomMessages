988f1ac056777082d989214380c99bbaf1e9198d
jeremiahMiclat <mic19017@byui.edu>
2024-05-26 19:00:33 +0800

    1st version

--- src/com/jeremiahMiclat/custommessages/CustomMessages.java
+++ src/com/jeremiahMiclat/custommessages/CustomMessages.java
@@ -1,15 +1,18 @@
 package com.jeremiahMiclat.custommessages;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomMessages extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new ProtectVillagerAlert(), this);
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "CustomMessages enabled!");
     }
 
     @Override
     public void onDisable() {
         // Any necessary cleanup
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "CustomMessages disabled!");
     }
 }
--- src/com/jeremiahMiclat/custommessages/ProtectVillagerAlert.java
+++ src/com/jeremiahMiclat/custommessages/ProtectVillagerAlert.java
@@ -2,14 +2,18 @@ package com.jeremiahMiclat.custommessages;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Server;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
+
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
 
 import static org.bukkit.Bukkit.getServer;
 
@@ -17,8 +21,7 @@ public class ProtectVillagerAlert implements Listener {
 
     @EventHandler
     public void onEntityTarget(EntityTargetEvent event) {
-        if (event.getTarget() instanceof Villager) {
-            Villager villager = (Villager) event.getTarget();
+        if (event.getTarget() instanceof Villager villager) {
             Location location = villager.getLocation();
             getServer().broadcastMessage(ChatColor.RED + "Villager "+ villager.getName() + " needs help! "+ event.getEntity() + " is attacking. Coords " + location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ());
         }
@@ -26,11 +29,42 @@ public class ProtectVillagerAlert implements Listener {
 
     @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent event) {
-        if (event.getEntity() instanceof Villager) {
-            Entity entity = event.getEntity();
-            Villager villager = (Villager) event.getEntity();
+        if (event.getEntity() instanceof Villager villager) {
+            Entity entity = event.getDamager();
             Location location = villager.getLocation();
-            getServer().broadcastMessage(ChatColor.RED + "Villager "+ villager.getName() + " needs help! "+ entity.getName() + " attacked. Coords " + location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ());
+            if (entity instanceof Player) {
+                getServer().broadcastMessage( ChatColor.LIGHT_PURPLE + "LOL " + entity.getName());
+            }
+            else {
+                getServer().broadcastMessage(ChatColor.RED + "Villager "+ villager.getName() + " needs help! "+ entity.getName() + " attacked. Coords " + location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (event.getEntity() instanceof Villager villager) {
+            Location location = villager.getLocation();
+
+            if (villager.getLastDamageCause() instanceof EntityDamageByEntityEvent damageEvent) {
+                Entity damager = damageEvent.getDamager();
+                if (damager instanceof Player player) {
+                    ItemStack weapon = player.getInventory().getItemInMainHand();
+                    String weaponName = weapon.hasItemMeta() && Objects.requireNonNull(weapon.getItemMeta()).hasDisplayName() ? weapon.getItemMeta().getDisplayName() : weapon.getType().toString();
+                    getServer().broadcastMessage(ChatColor.LIGHT_PURPLE+player.getName() + " killed a villager with " + weaponName + "!");
+                } else {
+                    getServer().broadcastMessage(ChatColor.RED+"A villager was killed by " + damager.getType().toString());
+                }
+
+            }
+
+            else {
+                getServer().broadcastMessage(ChatColor.RED + "Villager died. " + Objects.requireNonNull(villager.getLastDamageCause()).getCause() + " Coords " + location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ());
+            }
+
+
+
+
 
         }
     }
